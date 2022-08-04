83dcd39a8f931f4a5919a162aed9e6e75704736f
Soohan-Park <725psh@gmail.com>
2022-08-04 15:10:19 +0900

    Fix minor things

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "styles/theme";
-import Layout from "components/Layout";
+import Layout from "components/common/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
