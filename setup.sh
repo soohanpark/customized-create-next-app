243ccfa22690c7178bf682693f88832b7635fe62
Soohan-Park <725psh@gmail.com>
2024-02-03 16:11:06 +0900

    pref(script): improve setup logics

--- setup.sh
+++ setup.sh
@@ -1,16 +1,19 @@
 # Clean up
 rm -rf ./.git
 rm -rf ./setup.sh
+rm -rf ./README.md
 
 # Set
 currPath=$(pwd)
 dirname=${currPath##*/}
-echo "# $dirname" > ./README.md
+echo "# ${dirname}" > ./README.md
+sed "s/customized-create-next-app/${dirname}/g" package.json
+sed "s/\"version\": \"*\"/\"version\": \"0.0.0\"/g" package.json
 
 # Init
 git init
 git add README.md
-git commit -m "Init"
+git commit -m "init"
 
 # Done
 echo "Done!"
